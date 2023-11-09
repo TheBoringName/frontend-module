95970a674b58314450466104623e692609ff17cb
JMielczarek02 <102308814+JMielczarek02@users.noreply.github.com>
2023-11-09 17:47:27 +0100

    Add history component

---
+++ src/components/history/SingleEntry.jsx
@@ -0,0 +1,38 @@
+import styles from './SingleEntry.module.css'
+import arrow from '../../../public/dropdown.png'
+import arrowUp from '../../../public/dropup.png'
+import { useState } from 'react';
+
+const SingleEntry = ({object, status}) => {
+    const [isExtended, setIsExtended] = useState(false);
+    function dropDown(){
+        setIsExtended((prevValue)=>!prevValue);
+    }
+    return(
+        <>
+        <div className={styles.main}>
+
+            <p className={styles.title}>
+                {object.video_id}
+            </p>
+            <p className={styles.arrow}>
+                <img src={!isExtended? arrow: arrowUp} alt='scroll' onClick={dropDown}/>
+            </p>
+        </div>
+        {isExtended?
+        <div className={styles.description}>
+            {object.summary_text}
+            <p className={styles.date}>
+            {object.timestamp_generated}
+            </p>
+            <div className={styles.mood}>
+ 
+            <p>{status.grade}</p>
+            <p>{status.emoji}</p>
+            </div>
+         
+        </div>: null}
+        </>
+    )
+}
+export default SingleEntry
--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,6 +1,27 @@
+import SingleEntry from "../../components/history/SingleEntry";
+import responses from '../../../public/mocked_responses.json';
+import { Smiley, SmileyMeh, SmileySad } from '@phosphor-icons/react';
+
 const History = () => {
+  const status = [
+    {
+      grade: 'positive',
+      emoji: <Smiley size={60} color='#2f9e44'/>,
+    },
+    {
+      grade: 'neutral',
+      emoji: <SmileyMeh size={60} color='#868e96'/>,
+    },
+    {
+      grade: 'negative',
+      emoji: <SmileySad size={60} color='#e03131'/>,
+    },
+  ];
   return <>
     <h1>History</h1>
+    {responses.map((object, index)=>(
+      <SingleEntry object={object} status={status[object.summary_score]}key={index}/>
+    ))}
   </>
 }
 
