d36ec88ec94bbba51fb96560467532baf7c2b0dd
Ryszawy <Krafi604@gmail.com>
2023-11-20 18:19:06 +0100

    indenting fix

--- src/components/Upload/ProgressBar.jsx
+++ src/components/Upload/ProgressBar.jsx
@@ -1,10 +1,19 @@
 import styles from './ProgressBar.module.css';
-const ProgressBar = ({progress}) => {
-    return (
-        <><div className={styles['center']}>Analyzing Video </div><div className="progress"> 
-  <div className="progress-bar" role="progressbar" aria-valuenow={progress} aria-valuemin="0" aria-valuemax="100" style={{width: `${progress}%`}}></div>
-</div> </>
-    )
-
-}
+const ProgressBar = ({ progress }) => {
+  return (
+    <>
+      <div className={styles['center']}>Analyzing Video </div>
+      <div className="progress">
+        <div
+          className="progress-bar"
+          role="progressbar"
+          aria-valuenow={progress}
+          aria-valuemin="0"
+          aria-valuemax="100"
+          style={{ width: `${progress}%` }}
+        ></div>
+      </div>{' '}
+    </>
+  );
+};
 export default ProgressBar;
