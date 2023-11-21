f5cea46e877ba9ac508a04fe7f97b33d23b99e78
Ryszawy <Krafi604@gmail.com>
2023-11-21 11:18:08 +0100

    hero size 48em 62em

--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -1,23 +1,21 @@
-import { ReactSVG } from 'react-svg';
+// import { ReactSVG } from 'react-svg';
 import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <section className={styles['hero-box']}>
-      <div className="row">
-        <div className="col-8">
+    <section className={`${styles['hero-box']} padding-sides`}>
+      <div className={`${styles['grid-hero']} ${styles['grid--2-col']}`}>
+        <aside>
           <h2 className={styles.project}>What do they mean?</h2>
-          <p className="title">Lorem ipsum dolor!</p>
-          <p className="text">
+          <p className={styles['hero-box-title']}>Lorem ipsum dolor!</p>
+          <p className={styles['hero-box-text']}>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi minus
-            voluptatibus similique id aspernatur quia, incidunt officia ducimus
-            reiciendis libero veniam consequuntur doloremque quidem illo
-            quibusdam sunt magnam commodi. Nemo.
+            voluptatibus similique id aspernatur quia,
           </p>
-        </div>
-        <div className="col d-flex justify-content-end">
-          <ReactSVG src="/hero.svg" />
-        </div>
+        </aside>
+        <figure className={styles['hero-figure']}>
+          <img src="/hero.svg" className={styles['hero-img']} />
+        </figure>
       </div>
     </section>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
